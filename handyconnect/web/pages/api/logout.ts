bacdd0f7d1ffa8e37fe5f5ac4c735b8f011f2db2
Robert Matiwa <robertmatiwa3@gmail.com>
2025-11-10 17:29:52 +0200

    Add logout API route to clear authentication cookie

---
+++ handyconnect/web/pages/api/logout.ts
@@ -0,0 +1,12 @@
+﻿import type { NextApiRequest, NextApiResponse } from "next";
+import { clearAuthCookie } from "../../lib/auth";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end("Method Not Allowed");
+  }
+
+  clearAuthCookie(res);
+  return res.status(200).json({ success: true });
+}
